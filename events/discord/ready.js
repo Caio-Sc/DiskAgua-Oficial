121cd302542861366dc46fe1646a41053d4244a4
Caio-Sc <github.romp310@passinbox.com>
2025-08-08 16:40:34 -0300

    set presence

--- events/discord/ready.js
+++ events/discord/ready.js
@@ -1,12 +1,13 @@
-const { Events } = require("discord.js");
-
+const { Events, ActivityType, PresenceUpdateStatus } = require("discord.js");
 
 module.exports = {
   name: Events.ClientReady,
   once: true,
   execute(client) {
-	// IMPORTANTE: inicializa o manager com o ID do bot
+    // IMPORTANTE: inicializa o manager com o ID do bot
     client.manager.init(client.user.id);
+
+    client.user.setPresence({ activities: [{ name: 'música', type: ActivityType.Listening }], status: PresenceUpdateStatus.DoNotDisturb });
 
     console.log(`Ready! Logged in as ${client.user.tag}`);
   },
